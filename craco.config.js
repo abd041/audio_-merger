42a60ecf0a845fc8501c48f473ab166c3d057942
abd041 <abdullahrauf440@gmail.com>
2022-01-26 01:27:35 +0500

    yes

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,29 @@
 module.exports = {
-  devServer: {
+  webpack: {
     headers: {
+      "X-Frame-Options": "Deny",
+      "Access-Control-Allow-Origin": "*",
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
 };
 
+module.exports = {
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+};
 
 module.exports = {
   mode: "production",
   // Adding Server
   headers: {
+    "Access-Control-Allow-Origin": "*",
     "Cross-Origin-Embedder-Policy": "require-corp",
     "Cross-Origin-Opener-Policy": "same-origin",
   },
